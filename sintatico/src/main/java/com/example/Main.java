2b759ff13645dc4b1ad55a4f5080a432af45a285
Gabriel Marques Gallo <80481877+Gallo-10@users.noreply.github.com>
2024-03-04 02:10:32 -0300

    Analisador sintatico

---
+++ sintatico/src/main/java/com/example/Main.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        BenasLexer lexer = new BenasLexer(cs);
+
+//        // Descomentar para depurar o Léxico
+//        Token t = null;
+//        while( (t = lexer.nextToken()).getType() != Token.EOF) {
+//            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+//        }
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        BenasParser parser = new BenasParser(tokens);
+        parser.programa();
+    }
+}
