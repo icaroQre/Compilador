af481e77fe3543c321d81c018b2961ca04b8a9ea
icaroQre <icaro.queiroz.reccanello@gmail.com>
2024-03-04 17:51:13 -0300

    Atualizando nomes arquivos

---
+++ semantico/target/generated-sources/antlr4/com/example/OswaldoBaseVisitor.java
@@ -0,0 +1,113 @@
+// Generated from java-escape by ANTLR 4.11.1
+package com.example;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link OswaldoVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class OswaldoBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements OswaldoVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrograma(OswaldoParser.ProgramaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaracao(OswaldoParser.DeclaracaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpressaoAritmetica(OswaldoParser.ExpressaoAritmeticaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTermoAritmetico(OswaldoParser.TermoAritmeticoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFatorAritmetico(OswaldoParser.FatorAritmeticoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpressaoRelacional(OswaldoParser.ExpressaoRelacionalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitTermoRelacional(OswaldoParser.TermoRelacionalContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComando(OswaldoParser.ComandoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComandoAtribuicao(OswaldoParser.ComandoAtribuicaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComandoEntrada(OswaldoParser.ComandoEntradaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComandoSaida(OswaldoParser.ComandoSaidaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComandoCondicao(OswaldoParser.ComandoCondicaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitComandoRepeticao(OswaldoParser.ComandoRepeticaoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSubAlgoritmo(OswaldoParser.SubAlgoritmoContext ctx) { return visitChildren(ctx); }
+}
---
+++ semantico/target/generated-sources/antlr4/com/example/OswaldoListener.java
@@ -0,0 +1,150 @@
+// Generated from java-escape by ANTLR 4.11.1
+package com.example;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link OswaldoParser}.
+ */
+public interface OswaldoListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#programa}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrograma(OswaldoParser.ProgramaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#programa}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrograma(OswaldoParser.ProgramaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#declaracao}.
+	 * @param ctx the parse tree
+	 */
+	void enterDeclaracao(OswaldoParser.DeclaracaoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#declaracao}.
+	 * @param ctx the parse tree
+	 */
+	void exitDeclaracao(OswaldoParser.DeclaracaoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#expressaoAritmetica}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpressaoAritmetica(OswaldoParser.ExpressaoAritmeticaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#expressaoAritmetica}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpressaoAritmetica(OswaldoParser.ExpressaoAritmeticaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#termoAritmetico}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermoAritmetico(OswaldoParser.TermoAritmeticoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#termoAritmetico}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermoAritmetico(OswaldoParser.TermoAritmeticoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#fatorAritmetico}.
+	 * @param ctx the parse tree
+	 */
+	void enterFatorAritmetico(OswaldoParser.FatorAritmeticoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#fatorAritmetico}.
+	 * @param ctx the parse tree
+	 */
+	void exitFatorAritmetico(OswaldoParser.FatorAritmeticoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#expressaoRelacional}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpressaoRelacional(OswaldoParser.ExpressaoRelacionalContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#expressaoRelacional}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpressaoRelacional(OswaldoParser.ExpressaoRelacionalContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#termoRelacional}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermoRelacional(OswaldoParser.TermoRelacionalContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#termoRelacional}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermoRelacional(OswaldoParser.TermoRelacionalContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comando}.
+	 * @param ctx the parse tree
+	 */
+	void enterComando(OswaldoParser.ComandoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comando}.
+	 * @param ctx the parse tree
+	 */
+	void exitComando(OswaldoParser.ComandoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comandoAtribuicao}.
+	 * @param ctx the parse tree
+	 */
+	void enterComandoAtribuicao(OswaldoParser.ComandoAtribuicaoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comandoAtribuicao}.
+	 * @param ctx the parse tree
+	 */
+	void exitComandoAtribuicao(OswaldoParser.ComandoAtribuicaoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comandoEntrada}.
+	 * @param ctx the parse tree
+	 */
+	void enterComandoEntrada(OswaldoParser.ComandoEntradaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comandoEntrada}.
+	 * @param ctx the parse tree
+	 */
+	void exitComandoEntrada(OswaldoParser.ComandoEntradaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comandoSaida}.
+	 * @param ctx the parse tree
+	 */
+	void enterComandoSaida(OswaldoParser.ComandoSaidaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comandoSaida}.
+	 * @param ctx the parse tree
+	 */
+	void exitComandoSaida(OswaldoParser.ComandoSaidaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comandoCondicao}.
+	 * @param ctx the parse tree
+	 */
+	void enterComandoCondicao(OswaldoParser.ComandoCondicaoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comandoCondicao}.
+	 * @param ctx the parse tree
+	 */
+	void exitComandoCondicao(OswaldoParser.ComandoCondicaoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#comandoRepeticao}.
+	 * @param ctx the parse tree
+	 */
+	void enterComandoRepeticao(OswaldoParser.ComandoRepeticaoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#comandoRepeticao}.
+	 * @param ctx the parse tree
+	 */
+	void exitComandoRepeticao(OswaldoParser.ComandoRepeticaoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OswaldoParser#subAlgoritmo}.
+	 * @param ctx the parse tree
+	 */
+	void enterSubAlgoritmo(OswaldoParser.SubAlgoritmoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OswaldoParser#subAlgoritmo}.
+	 * @param ctx the parse tree
+	 */
+	void exitSubAlgoritmo(OswaldoParser.SubAlgoritmoContext ctx);
+}
